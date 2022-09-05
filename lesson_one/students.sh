701f5cc3647785f26ead2fe647e298a203602af6
eliza <Elizabeth>
2022-09-05 11:10:08 +0300

    add work

---
+++ lesson_one/students.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo -n "How many students?"
+echo ""
+read STUDENTS
+
+echo "$STUDENTS"
+
+case $STUDENTS in
+
+  0)
+    echo "No students"
+    ;;
+
+  1)
+    echo "1 student"
+    ;;
+
+  2)
+    echo "2 students"
+    ;;
+
+  3)
+    echo "3 students"
+    ;;
+  4)
+    echo "4 students"
+  ;;
+  *)
+    echo "A lot of students"
+    ;; 
+esac
